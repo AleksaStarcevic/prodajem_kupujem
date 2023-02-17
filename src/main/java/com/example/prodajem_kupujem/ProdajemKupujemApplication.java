478d4959eba9216c7f25657cbd54f02dc423862a
AleksaStarcevic <aleksastarcevic1@gmail.com>
2023-02-17 19:59:31 +0100

    init

---
+++ src/main/java/com/example/prodajem_kupujem/ProdajemKupujemApplication.java
@@ -0,0 +1,13 @@
+package com.example.prodajem_kupujem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdajemKupujemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdajemKupujemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prodajem_kupujem/ProdajemKupujemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prodajem_kupujem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdajemKupujemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
